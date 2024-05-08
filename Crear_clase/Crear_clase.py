f48fb313069ea3997ca063da88ad6d408ac97284
juan <Curum@DESKTOP-OJS0JKN>
2024-05-08 13:24:15 -0500

    metodo constructor __init__

--- Crear_clase/Crear_clase.py
+++ Crear_clase/Crear_clase.py
@@ -1,12 +1,18 @@
-# Adicionamos métodos
 
 import math
 
 # Creamos una clase
 class Vector:
-    # Creamos un metodo que recibe parametros
-    # esto facilita la inicializacion
-    def inicializa(self, x, y):
+    """
+    Un cosntructor es un método que se invoca automaticamente cuando
+    el objeto es creado/intanciado
+    El constructor en python es el metodo __new__, pero se usa solamente
+    en casos avanzados
+    El método __init__ tambien se invoco automaticamente en el proceso
+    de intanciación y es la opción preferida para llevar a cabo
+    las instancias del objeto en python
+    """
+    def __init__(self, x, y):
         # Colocamos los atributos
         self.x=x
         self.y=y
@@ -15,17 +21,18 @@ class Vector:
     def Muestra(self):
         print(self.x,self.y)
 
-
 # Y un método que regrese un valor
     def magnitud(self):
         return math.sqrt(self.x**2+self.y**2)
 
 # Creamos el objeto
-v1 = Vector()
+""" Ya no necesitamos invocar a inicializa manualmente
+simplemente pasamos los parametros al instanciar el objeto"""
+
+v1 = Vector(4,5)
 
 # Invocamos el metodo
-v1.inicializa(4,5)
-
+v1.Muestra()
 
 # Obtener la magnitud
 m=v1.magnitud()
