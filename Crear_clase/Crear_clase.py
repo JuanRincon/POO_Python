334f8de5a0258383e794852d06f43ae62da4975a
juan <Curum@DESKTOP-OJS0JKN>
2024-05-08 11:54:12 -0500

    Agregando métodos

--- Crear_clase/Crear_clase.py
+++ Crear_clase/Crear_clase.py
@@ -1,48 +1,32 @@
+# Adicionamos métodos
+
+import math
+
 # Creamos una clase
 class Vector:
-    # Creamos un metodo
-    # self es una referencia al objeto que invoca el metodo
-    # tambien se le conoce como variable de instancia
-    def inicializa(self):
+    # Creamos un metodo que recibe parametros
+    # esto facilita la inicializacion
+    def inicializa(self, x, y):
         # Colocamos los atributos
-        self.x=0
-        self.y=0
+        self.x=x
+        self.y=y
 
-# Creamos la instancia
-v1=Vector()
+    # Creamos otro método
+    def Muestra(self):
+        print(self.x,self.y)
+
+
+# Y un método que regrese un valor
+    def magnitud(self):
+        return math.sqrt(self.x**2+self.y**2)
+
+# Creamos el objeto
+v1 = Vector()
 
 # Invocamos el metodo
-v1.inicializa()
+v1.inicializa(4,5)
 
-# Accedemos a los atributos para imprimir sus contenidos
-print(v1.x,v1.y)
 
-# Coloca un valor en el atributo
-v1.x=5
-
-print(v1.x,v1.y)
-
-###
-
-# Creamos un segundo objeto
-v2=Vector()
-
-v2.inicializa()
-print(v2.x,v2.y)
-
-v2.x=3
-v2.y=7
-
-# Cada objeto tiene una vida por separado y guarda valores en sus
-# propios atributos
-print(v1.x,v1.y)
-print(v2.x,v2.y)
-
-# Una forma no recomendable de adicionar atributos
-v1.z=6
-
-print(v1.x, v1.y, v1.z)
-
-# El atributo no se comparte co la otra instancia, pues no esta
-# declrado en la clase
-# print(v2.x, v2.y, v2.z) /Por lo tanto esto último así como está nos saca un error
+# Obtener la magnitud
+m=v1.magnitud()
+print("La magnitud es", m)
