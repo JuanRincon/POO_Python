155f22fc0e4a1f5797b2dbf7c53975a60da6b122
juan <juan@DESKTOP-OJS0JKN.localdomain>
2024-08-17 20:10:19 -0500

    Clase hija Suma hereda de clase Operacion

--- herencia_simple.py
+++ herencia_simple.py
@@ -33,3 +33,25 @@ class Operacion:
 		print(numero)
 
 # Fin de la clase operación
+
+# Inicio de la clase hijo suma
+class Suma(Operacion):
+
+	# Se llama al constructor de la clase padre para iniciar los números
+	def __init__(self, numero1, numero2):
+		super().__init__(numero1, numero2)
+
+	# Se agrega un método nuevo a la clase suma(que no está en la clase padre)
+	def sumar(self):
+		self.imprimirValor(self.valor1 + self.valor2)
+
+# Fin de la clase suma
+
+# Crea de objeto de la clase hijo
+suma1 = Suma(10, 5)
+print("Operando 1 de la suma")
+suma1.imprimirValor(suma1.obtenerValor1())
+print("Operando 2 de la suma")
+suma1.imprimirValor(suma1.obtenerValor2())
+print("Resultado de la suma")
+suma1.sumar()
